6fba4cfdd633b79eb20eb66b2c7b173e7f8361f6
t0ottio13 <adsoption104@gmail.com>
2021-07-23 03:01:32 +0900

    ログイン機能追加しました！

---
+++ user_login.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>ログイン画面</title>
+</head>
+
+<body class="bg-light">
+    <div class="bg-white m-5 p-3 rounded">
+        <div class="mx-auto" style="width: 500px;">
+            <form action="./user_login_act.php" method="post" class="mt-5 mb-5">
+            <p class="h2 mt-4 fw-bold">ログインする</p>
+            <div class="mt-5 fs-4">
+                ユーザー名: <input type="text" name="user_name" class="border border-primary border-2 rounded">
+            </div>
+            <div class="mt-4 fs-4">
+            パスワード: <input type="password" name="password" class="border border-primary border-2 rounded">
+            </div>
+            <div>
+                <button type="submit" class="btn btn-primary mt-4 p-3">ログイン</button>
+            </div>
+            <div class="mt-4">
+                <p class="text-secondary d-inline">初めてのユーザーですか？</p>
+                <span><a href="./users/users_input.php" class="text-primary">新規登録</a></span>
+            </div>
+            <!-- <a href="todo_register.php">or register</a> -->
+            </form>
+        </div>
+    </div>
+</body>
+
+</html>
