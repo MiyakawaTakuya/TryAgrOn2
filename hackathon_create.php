07db52f34ae5a3cfff3dbe7fd4cfdf10ae43cef9
t0ottio13 <adsoption104@gmail.com>
2021-07-22 15:25:49 +0900

    アカウント登録画面作成

--- hackathon_create.php
+++ hackathon_create.php
@@ -11,7 +11,7 @@ if (
     !isset($_POST['event_date']) || $_POST['event_date'] == '' ||
     // 開催場所
     !isset($_POST['event_location']) || $_POST['event_location'] == '' ||
-    // 主催者
+    // 主催者名
     !isset($_POST['organizer_name']) || $_POST['organizer_name'] == '' ||
     // 課題館
     !isset($_POST['pain']) || $_POST['pain'] == '' ||
@@ -24,15 +24,39 @@ if (
     // 報酬・謝礼
     !isset($_POST['reward']) || $_POST['reward'] == '' ||
     // 参加方法
-    !isset($_POST['place']) || $_POST['place'] == ''
+    !isset($_POST['join_place']) || $_POST['join_place'] == '' ||
+    // 主催者id
+    !isset($_POST['organizer_id']) || $_POST['organizer_id'] == ''
 
 ) {
     echo json_encode(["error_msg" => "no input"]);
     exit();
 }
 
+// ハッカソン名
 $todo = $_POST['todo'];
-$deadline = $_POST['deadline'];
+// 開催予定日
+$event_date = $_POST['event_date'];
+// 開催場所
+$event_location = $_POST['event_location'];
+// 主催者名
+$organizer_name = $_POST['organizer_name'];
+// 課題館
+$pain = $_POST['pain'];
+// 期待すること
+$expectation = $_POST['expectation'];
+// 参加条件
+$requirements = $_POST['requirements'];
+// 参加者上限
+$upper_limit = $_POST['upper_limit'];
+// 報酬・謝礼
+$reward = $_POST['reward'];
+// 参加方法
+$join_place = $_POST['join_place'];
+// 主催者id
+$organizer_id = $_POST['organizer_id'];
+
+
 
 // $dbn = 'mysql:dbname=gsacf_l05_06;charset=utf8;port=3306;host=localhost';
 // $user = 'root';
@@ -45,12 +69,21 @@ $deadline = $_POST['deadline'];
 //   exit();
 // }
 
+$img = $_FILES['img']['name'];
+
+$upload = "./img/";
+if (move_uploaded_file($_FILES['img']['tmp_name'], $upload . $img)) {
+    echo 'アップロード成功';
+} else {
+    echo 'アップロード失敗';
+}
+
 
 $sql = 'INSERT INTO todo_table(id, todo, deadline, created_at, updated_at) VALUES(NULL, :todo, :deadline, sysdate(), sysdate())';
 
 $stmt = $pdo->prepare($sql);
 $stmt->bindValue(':todo', $todo, PDO::PARAM_STR);
-$stmt->bindValue(':deadline', $deadline, PDO::PARAM_STR);
+
 $status = $stmt->execute();
 
 if ($status == false) {
---
+++ users_input.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <title>新規ユーザー登録</title>
+</head>
+
+<body>
+        <form action="users_create.php" method="POST">
+        <div class="mt-4">
+            <input type="text" name="user_name" placeholder="氏名" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <input type="text" name="rubi_name" placeholder="フリガナ" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <input type="email" name="email" placeholder="メールアドレス" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <input type="text" name="tel" placeholder="電話番号" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <input type="number" name="age" placeholder="年齢" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <input type="password" name="password" placeholder="パスワード" class="p-2 rounded border-1" style="width: 500px;">
+        </div>
+        <div class="mt-4">
+            <button type="submit" class="btn btn-success" style="width: 500px;">アカウントを作成する</button>
+        </div>
+        <div class="mt-3 text-center">
+            <p class="d-inline text-secondary ">すでにアカウントをお持ちの方は</p>
+            <a class="d-inline text-secondary " href="../login.php">ログインへ</a>
+        </div>
+    </form>
+</body>
+
+</html>
+
+<!--
+user_id   INT INT(12) 主キー
+user_name VARCHAR(32)
+rubi_name VARCHARA(32)
+email VARCHARA(32)
+tel VARCHARA(11)
+age INT(2)
+password VARCHARA(32)
+
+is_deleted INT(1)
+created_at  DATETIME
+updated_at  DATETIME -->
