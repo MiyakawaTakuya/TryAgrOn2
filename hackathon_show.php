ad582162ffe1c2cd5d1b18ce7bc58d9dd296a5a9
Miyakawa Takuya <omyakawa@gmail.com>
2021-07-23 11:16:37 +0900

    Hakathon Details

--- hackathon_show.php
+++ hackathon_show.php
@@ -83,7 +83,7 @@ if ($status_member == false) {
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/show.css">
 
-    <title>ハッカソン詳細ページ</title>
+    <title>Hakathon Details</title>
 </head>
 
 <body class="body">
